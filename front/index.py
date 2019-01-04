3b98c97f3beadb462cf434df3ebb056f519b6dbc
root <root@ip-172-31-40-29.ap-northeast-1.compute.internal>
2019-01-04 03:14:52 +0000

    [UPD] http -> https

--- front/index.py
+++ front/index.py
@@ -1,6 +1,9 @@
 from flask import Flask, render_template
+import ssl
 
 app = Flask(__name__)
+context = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
+context.load_cert_chain('cert.crt', 'server_secret.key')
 
 @app.route("/")
 def home():
@@ -12,5 +15,5 @@ def english():
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0',debug=True)
-
+    #app.run(host='0.0.0.0',port=5000,ssl_context=context, threaded=True,debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
