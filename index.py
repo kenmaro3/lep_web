177de1c6ee0d38628866806f753e129f2586f680
kentaromihara <35465898+kentaromihara@users.noreply.github.com>
2019-01-12 18:36:46 +0900

    [ADD] line test and line_notice_english
    /line_notice_english is called by script.js when someone enters the english room,
    if the one subscribe the LINE bot, they get notified someone get in.

--- index.py
+++ index.py
@@ -1,7 +1,20 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, Response
+from flask import request, abort
+
+from linebot import (
+    LineBotApi, WebhookHandler
+)
+from linebot.exceptions import (
+    InvalidSignatureError
+)
+from linebot.models import (
+    MessageEvent, TextMessage, TextSendMessage,
+)
 #import ssl
 
 app = Flask(__name__)
+line_bot_api = LineBotApi(os.getenv("CHANNEL_ACCESS_TOKEN"))
+handler = WebhookHandler(os.getenv("CHANNEL_SECRET"))
 #context = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
 #context.load_cert_chain('cert.crt', 'server_secret.key')
 
@@ -13,6 +26,44 @@ def home():
 @app.route("/english")
 def english():
     return render_template("index.html")
+
+@app.route("/linetest")
+def linetest():
+    to = os.getenv("MY_USER_ID")
+    line_bot_api.push_message(to, TextSendMessage(text="hey, this is awesome!!"))
+#    return "seems good huh"
+
+@app.route("/line_notice_english", methods=['POST'])
+def line_notice_english():
+    to = os.getenv("MY_USER_ID")
+    line_bot_api.push_message(to, TextSendMessage(text="one learner came in English room!! You can also join from https://www.leplat4m.com/english"))
+    response = Response()
+    response.status_code=200
+    return response
+
+@app.route("/callback", methods=['POST'])
+def callback():
+    # get X-Line-Signature header value
+    signature = request.headers['X-Line-Signature']
+
+    # get request body as text
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
+
+    # handle webhook body
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        abort(400)
+
+    return 'OK'
+
+
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(text=event.message.text))
 
 
 if __name__ == "__main__":
