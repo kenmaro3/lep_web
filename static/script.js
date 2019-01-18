d3cb6c5f80fd8b23620e05be69bbcc932b518865
三原健太郎 <kmihara@MacBook-Pro-2.local>
2019-01-18 09:16:18 +0900

    [REF] Refactoring and commenting in the index.html

--- static/script.js
+++ static/script.js
@@ -1,89 +1,117 @@
 /* eslint-disable require-jsdoc */
 $(function() {
 
-let myId = null;
-let myRoom = null;
-roomMembers = [];
-const MAX_ROOM_NO = 5;
-const MAX_MEMBER = 2;
-let tryRoomNo = 1;
-let tryAutoJoin = false;
+  //====================================================================================================================
+  //for the room
+  let myId = null;
+  let myRoom = null;
+  roomMembers = [];
+  const MAX_ROOM_NO = 5;
+  const MAX_MEMBER = 2;
+  let tryRoomNo = 1;
+  let tryAutoJoin = false;
+
+  //create the room on top of the screen
+  console.log('before the for loop');
+  for(let i = 1; i <= MAX_ROOM_NO; i++){
+      const room = document.createElement('div');
+      const roomNameLabel = document.createElement('h3');
+      const roomMemberListTitle = document.createElement('div');
+      const roomMemberList = document.createElement('div');
+      const connectRoomButton = document.createElement('button');
+      const roomName = `Room_${i}`;
+      roomNameLabel.textContent = roomName;
+      roomMemberListTitle.textContent = 'ルームメンバー';
+      roomMemberList.id = `${roomName}MemberList`;
+      connectRoomButton.textContent = '入室';
+      connectRoomButton.dataset.roomName = roomName;
+      room.classList.add('room');
+      roomNameLabel.classList.add('room-name');
+      roomMemberListTitle.classList.add('room-memberlisttitle');
+      roomMemberList.classList.add('room-memberlist');
+      connectRoomButton.classList.add('connect-room-button');
+      room.appendChild(roomNameLabel);
+      room.appendChild(roomMemberListTitle);
+      room.appendChild(roomMemberList);
+      room.appendChild(connectRoomButton);
+      roomList.appendChild(room);
+
+      connectRoomButton.onclick = function(evt) {
+          joinRoom(this.dataset.roomName);
+      }
+  }
+  console.log('after the for loop');
 
 
-  console.log('at least starts here');
-  $.ajax({
-    url: '/line_notice_english',
-    type: 'post'
+  //  Auto join button handler
+  autoJoinButton.onclick = _ => {
+    addLog('Try Auto Join Start.');
+    tryRoomNo = 1;
+    autoJoinRoom();
+  }
+
+
+  //Show the room member handler
+  $("#roommember").click(function(){
+    console.log('roombutton is clicked');
+
+    console.log(roomMembers);
   });
 
 
+  //====================================================================================================================
+  //Line notification handler
+  //  console.log('at least starts here');
+  //  $.ajax({
+  //    url: '/line_notice_english',
+  //    type: 'post'
+  //  });
+
+  //====================================================================================================================
+  //  Translation button handler
+  $("#testbutton").click(function(){
+    console.log('button is clicked');
+
+    var textData = JSON.stringify({"text":$("#input-text").val()});
+    console.log(textData);
+
+    $.ajax({
+      url: '/toPostURL',
+      type: 'post',
+      data: textData,
+      contentType: 'application/json',
+
+      success:function(data){
+        console.log('i am called');
+        var result = JSON.parse(data.ResultSet).result;
+        console.log(result);
+        $("#result").text(result);
+      }
+
+    });
+
+  });
 
 
+  //====================================================================================================================
+  // New Peer object
+  const peer = new Peer({
+    key:   window.__SKYWAY_KEY__,
+    debug: 3,
+  });
 
-// Peer object
-const peer = new Peer({
-  key:   window.__SKYWAY_KEY__,
-  debug: 3,
-});
-
-addLog('Try Auto Join Start.');
-console.log('Try Auto Join Start.');
-tryRoomNo = 1;
+  addLog('Try Auto Join Start.');
+  console.log('Try Auto Join Start.');
+  tryRoomNo = 1;
 
 
   let localStream;
   let existingCall;
 
-
-      $("#roommember").click(function(){
-      console.log('roombutton is clicked');
-
-      console.log(roomMembers);
-
-
-
-    });
-
-
-
-
-    $("#testbutton").click(function(){
-      console.log('button is clicked');
-
-      var textData = JSON.stringify({"text":$("#input-text").val()});
-      console.log(textData);
-
-      $.ajax({
-        url: '/toPostURL',
-        type: 'post',
-        data: textData,
-        contentType: 'application/json',
-
-        success:function(data){
-          console.log('i am called');
-          var result = JSON.parse(data.ResultSet).result;
-          console.log(result);
-          $("#result").text(result);
-
-
-        }
-
-      });
-
-    });
-
-
-autoJoinButton.onclick = _ => {
-    addLog('Try Auto Join Start.');
-    tryRoomNo = 1;
-    autoJoinRoom();
-}
-
-
-
-
+  //  Peer Open
   peer.on('open', () => {
     myIdDisp.textContent = myId = peer.id;
+  //    Call the auto Join room
     autoJoinRoom();
     console.log("peer is open");
     $('#my-id').text(peer.id);
@@ -100,132 +128,104 @@ autoJoinButton.onclick = _ => {
     console.log('step1 was called');
   });
 
+  //====================================================================================================================
+  // Function for the autoJoinRoom
+  function autoJoinRoom() {
+    leaveMyRoom();
+    joinRoom(`Room_${tryRoomNo}`);
+    console.log('at the end of the autoJoinRoom');
+  }
 
-function autoJoinRoom() {
-  leaveMyRoom();
-  joinRoom(`Room_${tryRoomNo}`);
-  console.log('at the end of the autoJoinRoom');
-}
+  function joinRoom(roomName) {
+      console.log("joinRoom is called");
+      myRoom = peer.joinRoom(roomName);
+      myRoom.on('open', function() {
+          addRoomMember(this.name, myId);
+          addLog(`[${this.name}] Joined.`);
+      });
 
-function joinRoom(roomName) {
-    console.log("joinRoom is called");
-    myRoom = peer.joinRoom(roomName);
-    myRoom.on('open', function() {
-        addRoomMember(this.name, myId);
-        addLog(`[${this.name}] Joined.`);
-    });
+      console.log('checkpoint1');
+      myRoom.on('peerJoin', function(id) {
+          addRoomMember(this.name, id);
+          if(roomMembers.length > MAX_MEMBER) {
+              addLog(`Send abort to <${id}>.`);
+              this.send({abort:id});
+          }
+          addLog(`[${this.name}] Join Member <${id}]>.`);
+          this.send({member:myId});
+      });
 
-    console.log('checkpoint1');
-    myRoom.on('peerJoin', function(id) {
-        addRoomMember(this.name, id);
-        if(roomMembers.length > MAX_MEMBER) {
-            addLog(`Send abort to <${id}>.`);
-            this.send({abort:id});
-        }
-        addLog(`[${this.name}] Join Member <${id}]>.`);
-        this.send({member:myId});
-    });
+      console.log('checkpoint2');
+      myRoom.on('peerLeave', function(id) {
+          removeRoomMember(this.name, id);
+          addLog(`[${this.name}] Leave Member <${id}>.`);
+      });
 
-    console.log('checkpoint2');
-    myRoom.on('peerLeave', function(id) {
-        removeRoomMember(this.name, id);
-        addLog(`[${this.name}] Leave Member <${id}>.`);
-    });
+      console.log('checkpoint3');
+      myRoom.on('data', function(msg) {
+          addLog(`Message Received. ${JSON.stringify(msg.data)}`);
+          if(msg.data.member) {
+              addRoomMember(this.name, msg.data.member);
+              console.log('checkpoint4');
+          } else if(msg.data.abort && msg.data.abort === myId) {
+              leaveMyRoom();
+              tryRoomNo++;
+              console.log('checkpoint5');
+              if(tryRoomNo <= 10) {
+                  autoJoinRoom();
+              } else {
+                  addLog('Auto Join Failed.');
+              }
+          }
+      });
+  }
 
-    console.log('checkpoint3');
-    myRoom.on('data', function(msg) {
-        addLog(`Message Received. ${JSON.stringify(msg.data)}`);
-        if(msg.data.member) {
-            addRoomMember(this.name, msg.data.member);
-            console.log('checkpoint4');
-        } else if(msg.data.abort && msg.data.abort === myId) {
-            leaveMyRoom();
-            tryRoomNo++;
-            console.log('checkpoint5');
-            if(tryRoomNo <= 10) {
-                autoJoinRoom();
-            } else {
-                addLog('Auto Join Failed.');
-            }
-        }
-    });
-}
+  function leaveMyRoom() {
+      if(!myRoom) return;
+      roomMembers = [];
+      const roomName = myRoom.name;
+      myRoom.close();
+      myRoom = null;
+      removeRoomMember(roomName, myId);
+      const memberList = document.querySelector(`#${roomName}MemberList`);
+      if(memberList) memberList.innerHTML = '';
+      addLog(`[${roomName}] Leaved.`);
+  }
 
-function leaveMyRoom() {
-    if(!myRoom) return;
-    roomMembers = [];
-    const roomName = myRoom.name;
-    myRoom.close();
-    myRoom = null;
-    removeRoomMember(roomName, myId);
-    const memberList = document.querySelector(`#${roomName}MemberList`);
-    if(memberList) memberList.innerHTML = '';
-    addLog(`[${roomName}] Leaved.`);
-}
+  function addRoomMember(roomName, id) {
+      if(roomMembers.includes(id)) return;
+      roomMembers.push(id);
+      const roomMemberList = document.querySelector(`#${roomName}MemberList`);
+      const member = document.createElement('div');
+      member.id = `${roomName}_${id}`;
+      member.textContent = id;
+      roomMemberList.appendChild(member);
+  }
 
-function addRoomMember(roomName, id) {
-    if(roomMembers.includes(id)) return;
-    roomMembers.push(id);
-    const roomMemberList = document.querySelector(`#${roomName}MemberList`);
-    const member = document.createElement('div');
-    member.id = `${roomName}_${id}`;
-    member.textContent = id;
-    roomMemberList.appendChild(member);
-}
+  function removeRoomMember(roomName, id) {
+      const idx = roomMembers.indexOf(id);
+      if(idx !== -1) roomMembers.splice(idx, 1);
+      const roomMemberList = document.querySelector(`#${roomName}MemberList`);
+      const member = document.querySelector(`#${roomName}_${id}`);
+      if(member) member.remove();
+  }
 
-function removeRoomMember(roomName, id) {
-    const idx = roomMembers.indexOf(id);
-    if(idx !== -1) roomMembers.splice(idx, 1);
-    const roomMemberList = document.querySelector(`#${roomName}MemberList`);
-    const member = document.querySelector(`#${roomName}_${id}`);
-    if(member) member.remove();
-}
-
-function addLog(msg) {
-    const dt = new Date();
-    const time = [
-        `${dt.getHours()}`.padStart(2, '0'),
-        `${dt.getMinutes()}`.padStart(2, '0'),
-        `${dt.getSeconds()}`.padStart(2, '0')
-    ].join(':');
-    const div = document.createElement('div');
-    div.textContent = `[${time}] ${msg}`;
-//    logList.insertBefore(div, logList.firstChild);
-}
-
-
-console.log('before the for loop');
-for(let i = 1; i <= MAX_ROOM_NO; i++){
-    const room = document.createElement('div');
-    const roomNameLabel = document.createElement('h3');
-    const roomMemberListTitle = document.createElement('div');
-    const roomMemberList = document.createElement('div');
-    const connectRoomButton = document.createElement('button');
-    const roomName = `Room_${i}`;
-    roomNameLabel.textContent = roomName;
-    roomMemberListTitle.textContent = 'ルームメンバー';
-    roomMemberList.id = `${roomName}MemberList`;
-    connectRoomButton.textContent = '入室';
-    connectRoomButton.dataset.roomName = roomName;
-    room.classList.add('room');
-    roomNameLabel.classList.add('room-name');
-    roomMemberListTitle.classList.add('room-memberlisttitle');
-    roomMemberList.classList.add('room-memberlist');
-    connectRoomButton.classList.add('connect-room-button');
-    room.appendChild(roomNameLabel);
-    room.appendChild(roomMemberListTitle);
-    room.appendChild(roomMemberList);
-    room.appendChild(connectRoomButton);
-    roomList.appendChild(room);
-
-    connectRoomButton.onclick = function(evt) {
-        joinRoom(this.dataset.roomName);
-    }
-}
-console.log('after the for loop');
+  function addLog(msg) {
+      const dt = new Date();
+      const time = [
+          `${dt.getHours()}`.padStart(2, '0'),
+          `${dt.getMinutes()}`.padStart(2, '0'),
+          `${dt.getSeconds()}`.padStart(2, '0')
+      ].join(':');
+      const div = document.createElement('div');
+      div.textContent = `[${time}] ${msg}`;
+  //    logList.insertBefore(div, logList.firstChild);
+  }
 
 
-  console.log('call the roommenbers now');
+
+  //====================================================================================================================
+  //  Calling handler
   console.log(roomMembers);
   // Receiving a call
   peer.on('call', call => {
@@ -233,8 +233,6 @@ console.log('after the for loop');
     call.answer(localStream);
     step3(call);
   });
-
-  console.log("after the peer.on(call)");
 
   peer.on('error', err => {
     alert(err.message);
@@ -255,9 +253,6 @@ console.log('after the for loop');
     e.preventDefault();
     // Initiate a call!
     var callList = new Array();
-
-
-
       for (var i=0; i<roomMembers.length; i++){
 
         if(roomMembers[i] != peer.id){
@@ -296,11 +291,6 @@ console.log('after the for loop');
 
       f.on('error', err => alert(err));
     }
-
-
-
-
-
   });
 
   $('#end-call').on('click', () => {
@@ -486,22 +476,9 @@ console.log('after the for loop');
 
 
 
-
-//####################################
-const connectedPeers = {};
-
-  // Show this peer's ID.
-//  peer.on('open', id => {
-//    $('#pid').text(id);
-//  });
-//  // Await connections from others
-//  peer.on('connection', c => {
-//    // Show connection when it is completely ready
-//    c.on('open', () => connect(c));
-//  });
-//  peer.on('error', err => console.log(err));
-
-
+  //====================================================================================================================
+  //Chat handler
+  const connectedPeers = {};
 
   // Prepare file drop box.
   const box = $('#box');
